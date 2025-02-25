1db91b98df3c65be6dbe5313c4114b58c6a4458a
skbruno <bruno.rodriguesc9@gmail.com>
2025-02-25 12:30:05 -0300

    Add other entities and update database

--- SalesMVC/Data/SalesMVCContext.cs
+++ SalesMVC/Data/SalesMVCContext.cs
@@ -14,6 +14,8 @@ namespace SalesMVC.Data
         {
         }
 
-        public DbSet<SalesMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ SalesMVC/Models/SalesRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesMVC.Models.Enums;
+
+namespace SalesMVC.Models
+{
+    public class SalesRecord
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int iD, DateTime date, double amount, SalesStatus status, Seller seller)
+        {
+            ID = iD;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
