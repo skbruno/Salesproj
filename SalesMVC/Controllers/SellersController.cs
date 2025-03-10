3776794ac76db2af0110e9e928c39a615567301b
skbruno <bruno.rodriguesc9@gmail.com>
2025-03-10 17:09:02 -0300

    add seller create

--- SalesMVC/Controllers/SellersController.cs
+++ SalesMVC/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesMVC.Data;
 using SalesMVC.Services;
+using SalesMVC.Models;
 
 namespace SalesMVC.Controllers
 {
@@ -21,6 +22,19 @@ namespace SalesMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- SalesMVC/Services/SellerService.cs
+++ SalesMVC/Services/SellerService.cs
@@ -19,5 +19,12 @@ namespace SalesMVC.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            obj.Department = _context.Department.First();
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
